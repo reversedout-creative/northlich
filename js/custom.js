033e8e662b6b953f33ce4824609844e3e8aab6c0
metataggsolutions <metataggsolutions@gmail.com>
2017-09-12 19:10:36 +0530

    HTML Uploaded

---
+++ js/custom.js
@@ -0,0 +1,65 @@
+(function($) {
+	
+	/* Go up */
+	 jQuery(window).scroll(function () {
+	  if(jQuery(this).scrollTop() > 300 ) {
+	   jQuery(".go-up").css("bottom","0px");
+	  }
+	  else {
+	   jQuery(".go-up").css("bottom","-60px");
+	  }
+	  });
+	  jQuery(".go-up").click(function(){
+	   jQuery("html,body").animate({scrollTop:0},500);
+	   return false;
+	 });
+ 
+
+	/* Slime Menu */
+	$('.slimmenu').slimmenu({
+		resizeWidth: '991',
+		collapserTitle: '',
+		animSpeed:'medium',
+		indentChildren: true,
+		childrenIndenter: '<i class="fa fa-chevron-right"></i>'
+	});
+	// Close Effect Menu
+	$(function() {
+		$('.collapse-button').click(function() {
+			$('.collapse-button').toggleClass('close-menu');
+		});
+	});
+	
+	
+	/* --------------------------------------------------
+	 * document ready
+	 * --------------------------------------------------*/
+	jQuery(document).ready(function() {
+		$("body").show();
+		jQuery(window).on("scroll", function() {
+			/* fade base scroll position */
+			// custom page with background on side
+			jQuery('.side-bg').each(function() {
+				jQuery(this).find(".image-container").css("height", jQuery(this).find(".image-container").parent().css("height"));
+			});
+			/* go to anchor */
+		});
+	});
+	
+	/* WOW */
+	wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+	
+	/* END WOW */
+	
+	
+
+})(jQuery);
